ae3f3ad0f72ee720cccf49d0d61eb9a53736cf30
Daniel Marais <ST10132589@vcconnect.edu.za>
2024-09-21 12:36:54 +0200

    Created Announcement Model and Controller, added all methods for creating, getting, deleting and updating Announcemments

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const { createUser, createUserWithGoogle, checkGoogleUserExists, updateUserSettings, getUser, updateUser, getSettings,updateSettings } = require('./src/controllers/userController');
+const { createAnnouncement, deleteAnnouncement, getAnnouncements,getAnnouncementsByTeamId, updateViewCount } = require('./src/controllers/announcementController');
 const firebaseAdmin = require('./src/firebaseAdmin');
 
 // exports.helloWorld = functions.https.onCall((data, context) => {
@@ -17,3 +18,10 @@ exports.getUser = getUser;
 exports.updateUser = updateUser;
 exports.updateSettings = updateSettings;
 exports.getSettings = getSettings;
+
+//Create a new announcement
+exports.createAnnouncement = createAnnouncement;
+exports.deleteAnnouncement = deleteAnnouncement;
+exports.getAnnouncements = getAnnouncements;
+exports.getAnnouncementsByTeamId = getAnnouncementsByTeamId;
+exports.updateViewCount = updateViewCount;
